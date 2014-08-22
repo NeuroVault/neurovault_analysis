526f48461d44acc8105dd53f569d108b88e0b9eb
Gael Varoquaux <gael.varoquaux@normalesup.org>
2014-08-22 19:22:51 +0200

    Simplify the ICA script

--- plot_ica.py
+++ plot_ica.py
@@ -1,15 +1,24 @@
+"""
+Perform an ICA analysis on the NeuroVault data, and label the ICA
+maps using the labels inferred from the NeuroVault decoding.
+"""
+# Author: Gael Varoquaux
+# License: BSD
+
 import os
 
 import numpy as np
 import pandas as pd
+from scipy import stats
 
 from matplotlib import pyplot as plt
-from nilearn.input_data import NiftiMasker
 from sklearn.decomposition import FastICA
 
-# -------------------------------------------
-# loading data and metadata
+from nilearn.input_data import NiftiMasker
+from nilearn.plotting import plot_stat_map
 
+# -------------------------------------------
+# load data and metadata
 data_dir = "/tmp/neurovault_analysis"
 mask = 'gm_mask.nii.gz'
 
@@ -22,74 +31,40 @@ images = [os.path.join(data_dir, 'resampled',
 masker = NiftiMasker(mask=mask, memory=os.path.join(data_dir, 'cache'))
 X = masker.fit_transform(images)
 
-fast_ica = FastICA(n_components=20)
+fast_ica = FastICA(n_components=20, random_state=42)
 ica_maps = fast_ica.fit_transform(X.T).T
 
 ica_img = masker.inverse_transform(ica_maps)
 ica_img.to_filename('ica.nii.gz')
 
 # Use the terms from neurosynth to label the ICAs
-# We exclude 'action', as it is almost everywhere and is non descriptive
 terms = metadata[[c for c in metadata.columns
-                  if c.startswith('neurosynth decoding')
-                  and not 'action' in c]]
+                  if c.startswith('neurosynth decoding')]]
 terms = terms.fillna(0)
 term_matrix = terms.as_matrix()
-# Keep only the 10 first values per entry, as Neurosynth is not reliable
-# in the tails
-#for col in term_matrix:
-#    col[col < np.sort(col)[-10]] = 0
-term_matrix[term_matrix < .33] = 0
+# Labels that have a negative correlation are not present in the map
+term_matrix[term_matrix < 0] = 0
 
-# Not using the transform method as it shifts by the mean learned on
-# data
+# Don't use the transform method as it centers the data
 ica_terms = np.dot(term_matrix.T, fast_ica.components_.T).T
 col_names = [c[20:] for c in metadata.columns
-             if c.startswith('neurosynth decoding')
-             and not 'action' in c]
+             if c.startswith('neurosynth decoding')]
 
-# Hack to avoid having shorter names appear bigger
-max_size = max([len(n) for n in col_names])
-col_names_scaled = [n.center(max_size, '\xa0') for n in col_names]
+# -------------------------------------------
+# Generate figures
+for idx, (ic, ic_terms) in enumerate(zip(ica_maps, ica_terms)):
+    if -ic.min() > ic.max():
+        # Flip the map's sign for prettiness
+        ic = -ic
+        ic_terms = -ic_terms
 
-try:
-    # Generate figures
-
-    # word_cloud install via
-    # pip install --user git+https://github.com/amueller/word_cloud.git
-    import wordcloud
-    from scipy import stats
-    from nilearn.plotting import plot_stat_map
-
-    for idx, (ic, ic_terms) in enumerate(zip(ica_maps, ica_terms)):
-        if -ic.min() > ic.max():
-            # Flip the map's sign for prettiness
-            ic = -ic
-            ic_terms = -ic_terms
-        count_thr = stats.scoreatpercentile(np.abs(ic_terms), 75)
-        this_count = [(w, np.abs(c)) for w, c in zip(col_names_scaled, ic_terms)
-                      if np.abs(c) > count_thr]
-        colors = dict([(w, (255, 0, 0) if c > 0 else (0, 0, 255))
-                       for w, c in zip(col_names_scaled, ic_terms)
-                      if np.abs(c) > count_thr])
-        def color_func(word, *args):
-            return colors[word]
-
-        elements = wordcloud.fit_words(this_count, width=500, height=500,
-                                       prefer_horiz=.99)
-        # Draw the positioned words to a PNG file.
-        wordcloud.draw(elements, 'component_%i_terms.png' % idx,
-                       width=500, height=500, scale=2,
-                       color_func=color_func, bg_color=(255, 255, 255))
-
-        ic_thr = stats.scoreatpercentile(np.abs(ic), 85)
-        ic_img = masker.inverse_transform(ic)
-        important_words = np.array(col_names)[np.argsort(ic_terms)[-3:]]
-        plot_stat_map(ic_img, threshold=ic_thr,
-                      title=', '.join(important_words))
-        plt.savefig('component_%i_ic.png' % idx)
-except ImportError:
-    pass
+    ic_thr = stats.scoreatpercentile(np.abs(ic), 90)
+    ic_img = masker.inverse_transform(ic)
+    # Use the 4 terms weighted most as a title
+    important_terms = np.array(col_names)[np.argsort(ic_terms)[-4:]]
+    plot_stat_map(ic_img, threshold=ic_thr,
+                  title=', '.join(important_terms[::-1]))
+    plt.savefig('component_%i_ic.png' % idx)
+    plt.savefig('component_%i_ic.pdf' % idx)
 
 
-plt.show()
